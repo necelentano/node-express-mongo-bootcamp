8781a68d0ab315238298233e428ba0a44f887b5c
necelentano <necelentano@gmail.com>
2020-11-03 02:33:05 +0200

    Creating a Simple Web Server

---
+++ 01-node-farm/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+
+// SERVER
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server')
+})
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000')
+})
