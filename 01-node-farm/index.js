7e1e0339e38abb32461c73d34170dafb3d7a748b
necelentano <necelentano@gmail.com>
2020-11-03 03:15:04 +0200

    012-Routing

--- 01-node-farm/index.js
+++ 01-node-farm/index.js
@@ -1,9 +1,23 @@
 const http = require('http');
+const url = require('url');
 
 
 // SERVER
 const server = http.createServer((req, res) => {
-    res.end('Hello from the server')
+    
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW')
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT')
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-custom-header': 'Hello people who watch this code. Have a nice day!'
+        })
+        res.end('<h1>Page not found!</h1>');
+    }
 })
 
 server.listen(8000, '127.0.0.1', () => {
