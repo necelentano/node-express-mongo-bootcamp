22ae28fc91498c6897266df95cce66e9897219f8
necelentano <necelentano@gmail.com>
2020-11-14 03:44:12 +0200

    013-Building A Simple API

--- 01-node-farm/index.js
+++ 01-node-farm/index.js
@@ -1,8 +1,12 @@
 const http = require('http');
 const url = require('url');
-
+const fs = require('fs');
 
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     
     const pathName = req.url;
@@ -11,6 +15,9 @@ const server = http.createServer((req, res) => {
         res.end('This is the OVERVIEW')
     } else if (pathName === '/product') {
         res.end('This is the PRODUCT')
+    } else if (pathName === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'})
+        res.end(data)
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
