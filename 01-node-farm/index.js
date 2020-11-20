b2e50783a8d2eb44d2d218094b7367ecb495f71d
necelentano <necelentano@gmail.com>
2020-11-20 03:08:47 +0200

    016–Parsing Variables from URL

--- 01-node-farm/index.js
+++ 01-node-farm/index.js
@@ -28,10 +28,10 @@ const dataObject = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
     
-    const pathName = req.url;
+    const { query, pathname } = url.parse(req.url, true);
 
     // Overview page
-    if(pathName === '/' || pathName === '/overview') {
+    if(pathname === '/' || pathname === '/overview') {
         res.writeHead(200, {'Content-type': 'text/html'})
 
         const cardsHtml = dataObject.map(el => replaceTemplate(tempCard, el)).join('');
@@ -40,11 +40,14 @@ const server = http.createServer((req, res) => {
         res.end(output)
 
     // Product page
-    } else if (pathName === '/product') {
-        res.end('This is the PRODUCT')
+    } else if (pathname === '/product') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const product = dataObject[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output)
 
     // API
-    } else if (pathName === '/api') {
+    } else if (pathname === '/api') {
         res.writeHead(200, {'Content-type': 'application/json'})
         res.end(data)
     // Not found
