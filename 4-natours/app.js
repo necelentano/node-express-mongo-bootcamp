c398a8f9022a268427ec5c1e9c09292c1d45f6ae
necelentano <necelentano@gmail.com>
2021-01-11 22:07:34 +0200

    Starting Our API Handling GET Requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,18 +1,25 @@
+const fs = require('fs');
+
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res
-        .status(200)
-        .json({message: 'Hello from the server side!', app: 'Natours'});
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint ...')
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
+
 app.listen(port, () => {
-    console.log(`App running on port ${port} ...`);
+  console.log(`App running on port ${port} ...`);
 });
