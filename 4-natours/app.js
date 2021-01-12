00bb0835a321390de34426eec0b5d34e61e8f2eb
necelentano <necelentano@gmail.com>
2021-01-12 03:09:24 +0200

    Handling DELETE Requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -78,6 +78,21 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 3000;
 
 app.listen(port, () => {
