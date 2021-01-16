af65f9b5fa2f381ac7189b709951a8f508ab8686
necelentano <necelentano@gmail.com>
2021-01-16 19:59:37 +0200

    Serving Static Files

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -12,6 +12,8 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   console.log('Hello from the middleware ...');
   next();
