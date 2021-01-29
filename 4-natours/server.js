210d1808d3525cecc058d530a43a1ef35b095b73
necelentano <necelentano@gmail.com>
2021-01-29 02:09:39 +0200

    Creating a Simple Tour Model

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -22,6 +22,24 @@ const app = require('./app');
 
 //console.log(process.env);
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port} ...`);
