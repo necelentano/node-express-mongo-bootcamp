383c921ab40cba1dbd7ad7e9fae58707e4eaed33
necelentano <necelentano@gmail.com>
2021-02-06 02:30:52 +0200

    Making the API Better Aliasing and fix misspell in tourRoutes.js file name

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRouts');
+const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRouts');
 
 const app = express();
