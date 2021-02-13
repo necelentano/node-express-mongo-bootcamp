08ced855ecf553b8634b9118b8f87b0268cc014e
necelentano <necelentano@gmail.com>
2021-02-13 13:10:32 +0200

    Query Middleware

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -54,6 +54,10 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -80,6 +84,20 @@ tourSchema.pre('save', function (next) {
 //   console.log(document);
 //   next();
 // });
+
+// QUERY MIDDLEWARE
+// tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (documents, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  console.log(documents);
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
