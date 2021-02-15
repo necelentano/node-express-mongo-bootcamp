ff838efde8cb5c984697f8412c45a62ab5794ab9
necelentano <necelentano@gmail.com>
2021-02-15 20:50:17 +0200

    Handling Unhandled Routes

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -17,11 +17,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware ...');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -30,5 +25,12 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
