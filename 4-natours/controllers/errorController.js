8e5ff0e2b835d2be34815142ba9b8728be895c7d
necelentano <necelentano@gmail.com>
2021-02-21 22:09:08 +0200

    Handling Mongoose Validation Errors

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -11,6 +11,12 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data! ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -49,6 +55,8 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
     if (err.stack.startsWith('CastError')) error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (err.stack.startsWith('ValidationError'))
+      error = handleValidationErrorDB(error);
     sendErrorProd(error, res);
   }
 };
