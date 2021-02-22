d6b3a48e6176a8ec95f40040dd5e8550f8f339de
necelentano <necelentano@gmail.com>
2021-02-22 22:26:07 +0200

    Catching Uncaught Exceptions

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
