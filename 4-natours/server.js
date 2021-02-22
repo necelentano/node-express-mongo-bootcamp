adb2d228d56532476e69dc020dd22eedd7c8e505
necelentano <necelentano@gmail.com>
2021-02-22 21:20:25 +0200

    Errors Outside Express Unhandled Rejections

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -22,6 +22,14 @@ const app = require('./app');
 //console.log(process.env);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port} ...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
