eecc57b8649b0f2d8628761d5a1300a0163741d0
necelentano <necelentano@gmail.com>
2021-02-26 22:43:08 +0200

    Modelling Users

---
+++ 4-natours/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowecase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'A user must confirm a password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
