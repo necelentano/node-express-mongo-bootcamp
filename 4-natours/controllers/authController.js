e3cfba3e5ba69240d034aabe5ac9c24a295e11cc
necelentano <necelentano@gmail.com>
2021-03-05 22:40:32 +0200

    Protecting Tour Routes - Part 1

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -47,3 +47,25 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check if it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access!', 401)
+    );
+  }
+  // 2) Verification token
+
+  // 3) Check if user still exists
+
+  // 4) Check if user changed password after the token was issued
+  next();
+});
