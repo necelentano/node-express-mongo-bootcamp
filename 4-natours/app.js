6995ce759081574c535545034d1b9b946b7d54be
necelentano <necelentano@gmail.com>
2021-03-17 00:21:24 +0200

    Preventing Parameter Pollution

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -34,8 +35,24 @@ app.use(express.json({ limit: '10kb' }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
+
 // Data sanitization against XSS. Make sure this comes before any routes
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 
