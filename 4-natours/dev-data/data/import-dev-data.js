6fd511682cdd854d2c00296515527efd5b3ddd77
necelentano <necelentano@gmail.com>
2021-03-19 22:54:31 +0200

    Modelling Locations (Geospatial Data)

--- 4-natours/dev-data/data/import-dev-data.js
+++ 4-natours/dev-data/data/import-dev-data.js
@@ -21,9 +21,7 @@ mongoose
 
 // READ JSON FILE
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // IMPORT DATA INTO DB
 
