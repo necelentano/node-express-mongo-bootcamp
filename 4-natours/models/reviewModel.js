7f099539dbcdf18fda69db9e9b1a3381da699dd9
necelentano <necelentano@gmail.com>
2021-03-28 12:01:54 +0300

    Populating Reviews

--- 4-natours/models/reviewModel.js
+++ 4-natours/models/reviewModel.js
@@ -32,6 +32,17 @@ const reviewSchema = mongoose.Schema(
   }
 );
 
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: 'name photo',
+  });
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
