265c912f484a7c8e6d50eb446717bff5f3d5b387
necelentano <necelentano@gmail.com>
2021-04-05 21:39:33 +0300

    Adding a me Endpoint

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -14,6 +14,13 @@ router.patch(
   authController.protect,
   authController.updatePassword
 );
+
+router.get(
+  '/me',
+  authController.protect,
+  userController.getMe,
+  userController.getUser
+);
 router.patch('/updateMe', authController.protect, userController.updateMe);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
