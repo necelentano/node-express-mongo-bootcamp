8d6dc05a99dee46466f80c6bcbe8ab7926fc1ad3
necelentano <necelentano@gmail.com>
2021-04-06 00:54:54 +0300

    Adding Missing Authentication and Authorization

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -9,11 +9,11 @@ router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+
+// Protect all routes after this middleware
+router.use(authController.protect);
+
+router.patch('/updateMyPassword', authController.updatePassword);
 
 router.get(
   '/me',
@@ -21,8 +21,10 @@ router.get(
   userController.getMe,
   userController.getUser
 );
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
