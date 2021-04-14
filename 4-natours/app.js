c05fb0762bd33442a4b08b358dcb903cd6ed2325
necelentano <necelentano@gmail.com>
2021-04-14 22:01:05 +0300

    Extending Our Base Template with Blocks

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -73,6 +73,19 @@ app.get('/', (req, res) => {
     user: 'Jonas',
   });
 });
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
