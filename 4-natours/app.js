40a05e5734f41ff799d1abb919f50c89faae01eb
necelentano <necelentano@gmail.com>
2021-04-14 16:49:09 +0300

    First Steps with Pug

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -68,7 +68,10 @@ app.use((req, res, next) => {
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas',
+  });
 });
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
