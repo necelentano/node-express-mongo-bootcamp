377cc7a5420a19fe1903f47c597b512f9acbe256
necelentano <necelentano@gmail.com>
2021-04-16 23:56:56 +0300

    Building the Tour Page - Part 1

--- 4-natours/controllers/viewsController.js
+++ 4-natours/controllers/viewsController.js
@@ -12,8 +12,15 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) Get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
   });
-};
+  // 2) Build template
+  // 3) Render template using data from 1)
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
