509c3a9f986adf7da849adea78de0c31f34bf10a
necelentano <necelentano@gmail.com>
2021-05-12 18:36:45 +0300

    Image Uploads Using Multer Users

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -19,6 +19,9 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
+  console.log(req.file);
+  console.log(req.body);
+
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm)
     return next(
